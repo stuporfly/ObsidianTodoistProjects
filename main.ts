0d38f4fd01571fb2baab1d8684b389f5af853ee2
Jonas Dam <msn@fnord.dk>
2024-07-16 22:31:48 +0200

    cleanup after code review

--- main.ts
+++ main.ts
@@ -1,10 +1,9 @@
-import { App, Editor, FileManager, FileSystemAdapter, FrontMatterCache, MarkdownView, Modal, normalizePath, Notice, Plugin, PluginSettingTab, Setting, TFile, TFolder } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, normalizePath, Notice, Plugin, PluginSettingTab, Setting, TFile, TFolder } from 'obsidian';
 import { Project, TodoistApi } from "@doist/todoist-api-typescript"// Remember to rename these classes and interfaces!
-import { Console } from 'console';
 const os = require('os');
 // var defaultTemplate="[{{projectName}}](https://todoist.com/app/project/{{projectId}})\n```todoist \n\"name\": \"{{projectName}}\"\n \"filter\": \"#{{projectName}}\"\n```\n";
 
-interface MyPluginSettings {
+interface TodoistProjectSyncSettings {
 	PrimarySyncDevice: string;
 	TodoistSyncFrequency: number;
 	TodoistToken: string;
@@ -12,7 +11,7 @@ interface MyPluginSettings {
 	// TodoistPageTemplate:string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: TodoistProjectSyncSettings = {
 	TodoistToken: 'default',
 	TodoistProjectFolder: 'Projects',
 	// TodoistPageTemplate:defaultTemplate,
@@ -20,70 +19,11 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 	PrimarySyncDevice:''
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class TodoistProjectSync extends Plugin {
+	settings: TodoistProjectSyncSettings;
 	
 	async onload() {
 		await this.loadSettings();
-		
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-		
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-		
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-					
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-		
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-		
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-		// 	console.log('click', evt);
-		// });
 		
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(async () => {
@@ -102,33 +42,30 @@ console.log(new Date().toLocaleString()+': Todoist Project files updated');
 //My result:
 
 //app:reload
-var folder=	 this.app.vault.getAbstractFileByPath(normalizePath("/TodoistProjects/Home improvement/build Wall"));
 const api = new TodoistApi(this.settings.TodoistToken);
 if (!await this.app.vault.adapter.exists(this.settings.TodoistProjectFolder))
 this.app.vault.createFolder(this.settings.TodoistProjectFolder);
 api.getProjects()
 	.then((projects: Project[]) => {
 
-		var 	reloadNeeded=false;
-		var cleanupDone=false;
-		var files = this.app.vault.getFiles();
-		var filesById: { [id: string] : TFile; } = {};
+		const files = this.app.vault.getFiles();
+		const filesById: { [id: string] : TFile; } = {};
 		
 		files.forEach(file => {
-			var Metadata = this.app.metadataCache.getFileCache(file);
+			const Metadata = this.app.metadataCache.getFileCache(file);
 if (Metadata?.frontmatter?.TodoistId)
 									filesById[Metadata?.frontmatter?.TodoistId]=file;
 			
 		});
-var  handledProjects  :string[]= [];
+const  handledProjects  :string[]= [];
 		projects.forEach(async element => {
 			handledProjects.push(element.id);
-			var filepath = this.getPath(projects, element.id);
+			const filepath = this.getPath(projects, element.id);
 			if (!await this.app.vault.adapter.exists(this.settings.TodoistProjectFolder+filepath))
 			await this.app.vault.createFolder(this.settings.TodoistProjectFolder+filepath);
 
-			var filename = this.settings.TodoistProjectFolder +filepath+ '/' + element.name + '.md';
-var currentfile:TFile;
+			const filename = this.settings.TodoistProjectFolder +filepath+ '/' + element.name + '.md';
+
 			if (files.filter(file => file.path == filename).length == 0 ) {
 				if (!filesById[element.id])
 				{
@@ -137,20 +74,20 @@ var currentfile:TFile;
 				}
 				else
 				{
-					var oldPath = "/"+filesById[element.id].path.substring(0,filesById[element.id].path.length-(element.name+".md").length-1);
+					const oldPath = "/"+filesById[element.id].path.substring(0,filesById[element.id].path.length-(element.name+".md").length-1);
 					if (!(await this.app.vault.adapter.exists("/"+filename)) &&(await this.app.vault.adapter.exists("/"+filesById[element.id].path)))
 {
 	
 					await this.app.vault.rename(filesById[element.id],filename);
-				
-					var folderToDelete=	 this.app.vault.getAbstractFileByPath(normalizePath(oldPath)) as TFolder;
-					var keepDeleting=true;
+					if ( this.app.vault.getAbstractFileByPath(normalizePath(oldPath)) instanceof TFolder)
+						{					
+							let folderToDelete=	 this.app.vault.getAbstractFileByPath(normalizePath(oldPath)) as TFolder  ;
+		let keepDeleting=true;
 					if (folderToDelete.children.length==0)
 {								while (keepDeleting)
 					{
-						var nextfolderToDelete=	 folderToDelete?.parent;
-							await this.app.vault.delete(folderToDelete!!);
-							reloadNeeded=true;
+						const nextfolderToDelete=	 folderToDelete?.parent;
+							await this.app.fileManager.trashFile(folderToDelete!!);
 							folderToDelete=nextfolderToDelete!!;
 							if (folderToDelete.children.length>0)
 								keepDeleting=false;
@@ -159,18 +96,16 @@ var currentfile:TFile;
 						}
 					}
 				}
+				}
 			}
-			cleanupDone=true;
 		});
-		var filesToRemove:TFile[]=[];
+		const filesToRemove:TFile[]=[];
 		
-		var testArray= ["test","test2"];
 		files.forEach(file=>{
-			var test = "";
-			var Metadata = this.app.metadataCache.getFileCache(file);
+			const Metadata = this.app.metadataCache.getFileCache(file);
 			if (Metadata?.frontmatter?.TodoistId)
 			{		
-				var todoistId:string=Metadata?.frontmatter?.TodoistId!!;
+				const todoistId:string=Metadata?.frontmatter?.TodoistId;
 				
 				
 				if (!handledProjects.contains( todoistId.toString()))
@@ -183,9 +118,9 @@ var currentfile:TFile;
 			if (!await this.app.vault.adapter.exists(this.settings.TodoistProjectFolder+"/archive"))
 			await this.app.vault.createFolder(this.settings.TodoistProjectFolder+"/archive");
 			
-			var Metadata = this.app.metadataCache.getFileCache(file);
-			var projectNameFromMeta=Metadata?.frontmatter?.projectName;
-			var todooistId=Metadata?.frontmatter?.TodoistId;
+			const Metadata = this.app.metadataCache.getFileCache(file);
+			const projectNameFromMeta=Metadata?.frontmatter?.projectName;
+			const todooistId=Metadata?.frontmatter?.TodoistId;
 			if (!projectNameFromMeta)
 			await this.addYamlProp( "projectName",  file.name,  file);
 			await this.app.vault.rename(file,this.settings.TodoistProjectFolder+"/archive/"+todooistId+".md");
@@ -199,11 +134,11 @@ else
 console.log("Not Primary sync device - skipping Todoist sync");
 }
 getPath(projects: Project[], currentProjectId?: string): string {
-	var result = "";
+	let result = "";
 	if (currentProjectId) {
-		var currentProject = projects.find(p => p.id === currentProjectId);
+		const currentProject = projects.find(p => p.id === currentProjectId);
 		if (currentProject?.parentId) {
-			let parentProj = projects.find((proj: Project) => proj.id === currentProject?.parentId);
+			const parentProj = projects.find((proj: Project) => proj.id === currentProject?.parentId);
 			result = this.getPath(projects, parentProj?.id) + "/"+parentProj?.name;
 		}
 	}
@@ -212,11 +147,10 @@ getPath(projects: Project[], currentProjectId?: string): string {
 }
 public async addYamlProp(propName: string, propValue: string, file: TFile): Promise<void> {
 	const fileContent: string = await this.app.vault.read(file);
-	const frontmatter: FrontMatterCache = this.app.metadataCache.getFileCache(file)!!.frontmatter!!;
-	const isYamlEmpty: boolean = (frontmatter === undefined && !fileContent.match(/^-{3}\s*\n*\r*-{3}/));
+	const isYamlEmpty: boolean = (this.app.metadataCache.getFileCache(file)?.frontmatter === undefined && !fileContent.match(/^-{3}\s*\n*\r*-{3}/));
 	
 	
-	let splitContent = fileContent.split("\n");
+	const splitContent = fileContent.split("\n");
 	if (isYamlEmpty) {
 		splitContent.unshift("---");
 		splitContent.unshift(`${propName}: ${propValue}`);
@@ -243,79 +177,7 @@ async saveSettings() {
 }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-	
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!');
-	}
-	
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
-}
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-	
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-	
-	display(): void {
-		const { containerEl } = this;
-		
-		containerEl.empty();
-		
-		containerEl.createEl('h2', { text: 'Settings for Todoist plugin.' });
-		
-		new Setting(containerEl)
-			.setName('Todoist API Key')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.TodoistToken)
-				.onChange(async (value) => {
-					this.plugin.settings.TodoistToken = value;
-					await this.plugin.saveSettings();
-				}));
-				new Setting(containerEl)
-				.setName('Todoist project Folder')
-				.setDesc('folder for projects' )
-				.addText(text => text
-					.setPlaceholder('enter path')
-					.setValue(this.plugin.settings.TodoistProjectFolder)
-					.onChange(async (value) => {
-						this.plugin.settings.TodoistProjectFolder = value;
-						await this.plugin.saveSettings();
-					}));
-					new Setting(containerEl)
-					.setName('Primary sync device')
-					.setDesc('if this field is set, projects will only sync on the device with this name. This is to prevent sync-problems if projects are updated on multiple devices. The name of this device is"' + os.hostname()+'".')
-					.addText(text => text
-						.setPlaceholder('')
-						.setValue(this.plugin.settings.PrimarySyncDevice)
-						.onChange(async (value) => {
-							this.plugin.settings.PrimarySyncDevice = value;
-							await this.plugin.saveSettings();
-						}));
-						new Setting(containerEl)
-			.setName('Todoist sync frequency in seconds')
-			.setDesc('Sync frequency in seconds')
-			.addText(Number => Number
-				.setPlaceholder("0")
-				.setValue(this.plugin.settings.TodoistSyncFrequency.toString())
-				.onChange(async (value) => {
-					this.plugin.settings.TodoistSyncFrequency = parseInt(value);
-					await this.plugin.saveSettings();
-				}));
-	
-	}
-}
+
 
 
