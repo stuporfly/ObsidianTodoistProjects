89eb9cc293ec074cc4618972f40e8a1da4dfd445
Jonas Dam <msn@fnord.dk>
2024-07-22 22:59:25 +0200

    removed sample stuff

--- main.ts
+++ main.ts
@@ -22,20 +22,6 @@ export default class TodoistProjectSync extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new TodoistSettingTab(this.app, this));
 
